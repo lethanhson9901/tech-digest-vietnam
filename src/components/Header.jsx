73dca53ee45aabc9890025c89b3b85a76795f537
lethanhson9901 <hoasung9901@gmail.com>
2025-11-26 10:46:16 +0700

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -158,7 +158,7 @@ const Header = ({ toggleDarkMode, isDarkMode }) => {
       dropdownItems: [
         {
           path: '/quick-view',
-          label: 'Tóm tắt nhanh',
+          label: 'Tin nhanh',
           icon: (
             <svg className="w-4 h-4 mr-2 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
